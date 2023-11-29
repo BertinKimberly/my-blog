9c595bc3d3bd235e2e66e994d08d23da1e128c7e
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-11-29 21:48:25 +0200

    added a navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,29 @@
 import Link from "next/link";
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
    return (
-      <nav className='container h-[100px] mx-auto flex items-center'>
+      <nav className='container h-[100px] mx-auto flex items-center justify-between'>
          <div>
             <Link href='/'>BERTIN</Link>
+         </div>
+         <div>
+            <form className='flex'>
+               <button className='w-[30px] h-[30px] border border-gray-600 flex items-center justify-center rounded-full z-10'>
+                  <FaSearch />
+               </button>
+               <input
+                  type='text'
+                  className='w-[200px] border-t border-r border-b rounded-full border-gray-600 -ml-7 pl-8'
+               />
+            </form>
+         </div>
+         <div className='flex gap-5'>
+            <Link href='/'>Home</Link>
+            <Link href='/'>About</Link>
+            <Link href='/'>Contact</Link>
+            <Link href='/'>Posts</Link>
          </div>
       </nav>
    );
