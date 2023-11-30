1285182a7f1b281243b336885ea4713cf1e30cf1
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-11-30 07:57:29 +0200

    made navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
+"use client";
 import Link from "next/link";
-import React from "react";
-import { FaSearch } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaBars, FaSearch, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+   const [isToggleMenu, setIsToggleMenu] = useState(false);
+   const toggleMenu = () => {
+      setIsToggleMenu((prev) => !prev);
+   };
    return (
-      <nav className='container h-[100px] mx-auto flex items-center justify-between'>
+      <nav className='container h-[100px] mx-auto flex items-center justify-between relative'>
          <div>
             <Link href='/'>BERTIN</Link>
          </div>
@@ -15,15 +20,27 @@ const Navbar = () => {
                </button>
                <input
                   type='text'
-                  className='w-[200px] border-t border-r border-b rounded-full border-gray-600 -ml-7 pl-8'
+                  className='w-[100px] sm:w-[200px] border-t border-r border-b rounded-full border-gray-600 -ml-7 pl-8'
                />
             </form>
          </div>
-         <div className='flex gap-5'>
+         <div
+            className={`${
+               isToggleMenu
+                  ? "flex flex-col gap-3 text-2xl absolute right-2 top-[100px] bg-white z-20"
+                  : "hidden"
+            }  md:flex gap-5`}
+         >
             <Link href='/'>Home</Link>
             <Link href='/'>About</Link>
             <Link href='/'>Contact</Link>
             <Link href='/'>Posts</Link>
+         </div>
+         <div
+            className='md:hidden block'
+            onClick={toggleMenu}
+         >
+            {isToggleMenu ? <FaTimes /> : <FaBars />}
          </div>
       </nav>
    );
