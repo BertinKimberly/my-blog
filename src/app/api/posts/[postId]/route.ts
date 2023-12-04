cbda890d4970c1bdc534f528fb3443e5a9f909cd
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-12-04 21:57:57 +0200

    updated api

---
+++ src/app/api/posts/[postId]/route.ts
@@ -0,0 +1,37 @@
+import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
+
+
+
+
+export async function POST(
+  request: Request, 
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return console.log('Hi');
+    
+  }
+
+  const body = await request.json();
+  const { 
+    name,
+    description,
+    imageSrc
+   } = body;
+
+
+
+  const listing = await prisma.listing.create({
+    data: {
+      name,
+      imageSrc,
+      description,
+      userId:currentUser.id
+    //   userId: currentUser.id
+    }
+  });
+
+  return NextResponse.json(listing);
+}
---
+++ src/utils/auth.ts
@@ -0,0 +1,25 @@
+
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "./connect";
+import { getServerSession, GetServerSessionOptions } from "next-auth";
+
+const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+};
+
+interface GetAuthSessionOptions extends GetServerSessionOptions {
+  // Add any additional options you might need
+}
+
+export const getAuthSession = () => getServerSession(authOptions as GetAuthSessionOptions);
